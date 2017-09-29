46ca3336140bd86a6de194549a0c7951688c5a0d
LachieBunker <Lachie.Bunker64@yahoo.com.au>
2017-09-29 13:08:07 +1000

    Testing java

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 document.addEventListener('main', function() {
 
   var testButton = document.getElementById('temp');
+  var colorButton = document.getElementById('Color');
 
   function openWebsite()
   {
     chrome.tabs.create({url:"http://www.qartwheelproductions.com/antidote/"})
+  }
+
+  function changeHTML()
+  {
+    colorButton.style.background-color = "blue";
   }
 
   testButton.addEventListener('click', function() {
