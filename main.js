3e7b05a65aaec313909627a62d20724264e26112
LachieBunker <Lachie.Bunker64@yahoo.com.au>
2017-10-26 21:22:18 +1000

    Updating Antidote page

--- main.js
+++ main.js
@@ -63,6 +63,6 @@ function hidePanels()
 function slideshowButtonClicked(slideshowImage)
 {
   var sPanel = document.getElementById('slideshow-panel');
-  var slideshowImageUrl = "url('/ArtAssets/SkillsButton1_Default.png')";
+  var slideshowImageUrl = "url('/ArtAssets/'" + slideshowImage + "'.png')";
   sPanel.style.backgroundImage = slideshowImageUrl;
 }
