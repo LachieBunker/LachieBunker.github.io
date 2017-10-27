b4b32bc7231d31ff08f829102a858251903368c4
LachieBunker <Lachie.Bunker64@yahoo.com.au>
2017-10-27 10:24:05 +1000

    Updating Gun Runners page

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ function hidePanels()
 function slideshowButtonClicked(slideshowImage)
 {
   var sPanel = document.getElementById('slideshow-panel');
-  var slideshowImageUrl = "url('/ArtAssets/" + slideshowImage + ".png')";
+  var slideshowImageUrl = "url('/ArtAssets/" + slideshowImage + ")";
   sPanel.style.backgroundImage = slideshowImageUrl;
 
 }
